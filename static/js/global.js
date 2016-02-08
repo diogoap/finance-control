d55cd708bbc5dee911e830ffeae437a72c1e8bec
Diogo Augusto Pereira <diogoap82@gmail.com>
2016-02-08 21:41:02 -0200

    Adding Service worker

---
+++ static/js/global.js
@@ -0,0 +1,29 @@
+(function() {
+	$(window).load(function(){
+		$('.navbar-collapse a').click(function(){
+			$(".navbar-collapse").collapse('hide');
+		});
+
+		$('.navbar-header a').click(function(){
+			$(".navbar-collapse").collapse('hide');
+		});
+	});
+
+	$(window).ready(function(){
+		$('.date-mask').mask("00/00/00");
+	});
+
+	// ServiceWorker is a progressive technology. Ignore unsupported browsers
+	if ('serviceWorker' in navigator) {
+		console.log('CLIENT: service worker registration in progress.');
+
+		navigator.serviceWorker.register('serviceWorker.js').then(function() {
+			console.log('CLIENT: service worker registration complete.');
+		}, function() {
+			console.log('CLIENT: service worker registration failure.');
+		});
+	} else {
+		console.log('CLIENT: service worker is not supported.');
+	}
+
+})();
