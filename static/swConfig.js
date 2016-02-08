4bbe2f014619008c1d5de048b27462a55dd0f5d3
Diogo Augusto Pereira <diogoap82@gmail.com>
2016-02-08 21:10:55 -0200

    Adding Service worker

--- static/swConfig.js
+++ static/swConfig.js
@@ -1,8 +1,10 @@
 this.addEventListener('install', function(event) {
+	console.log('addEventListener - install');
 	event.waitUntil(
-		caches.open('v1').then(function(cache) {
+		caches.open('v2').then(function(cache) {
 			return cache.addAll([
 				'/css/',
+				'/css/app.css',
 				'/html/',
 				'/images/',
 				'/js/',
@@ -13,13 +15,16 @@ this.addEventListener('install', function(event) {
 });
 
 this.addEventListener('fetch', function(event) {
+	console.log('addEventListener - fetch');
+
 	var response;
 
 	event.respondWith(caches.match(event.request).catch(function() {
+		console.log('Hello from SW');
 		return fetch(event.request);
 	}).then(function(r) {
 		response = r;
-		caches.open('v1').then(function(cache) {
+		caches.open('v2').then(function(cache) {
 			cache.put(event.request, response);
 		});
 		return response.clone();
