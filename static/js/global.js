6375572bb316aa7eba1552682563d06f06409d61
Diogo Augusto Pereira <diogoap82@gmail.com>
2016-02-08 23:52:47 -0200

    Adding Service worker

--- static/js/global.js
+++ static/js/global.js
@@ -15,10 +15,7 @@
 
 	// ServiceWorker is a progressive technology. Ignore unsupported browsers
 	if ('serviceWorker' in navigator) {
-		console.log('CLIENT: service worker registration in progress.');
-
 		navigator.serviceWorker.register('serviceWorker.js').then(function() {
-			console.log('CLIENT: service worker registration complete.');
 		}, function() {
 			console.log('CLIENT: service worker registration failure.');
 		});
